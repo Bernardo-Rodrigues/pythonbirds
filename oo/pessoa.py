27e9ece45e9868fb5c266b45bc7d3e11526fab55
Bernardo-Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2021-07-08 20:18:16 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,16 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = 19):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Rodrigues')
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Bernardo'
+    print(p.nome)
+    print(p.idade)
