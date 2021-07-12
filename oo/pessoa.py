d7a4728e84b61beff42dc37c51955ed25f91a14e
Bernardo-Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2021-07-12 19:07:32 -0300

    Criado e removido atributo dinâmico de objetos do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,21 @@
 class Pessoa:
-    def __init__(self, nome = None, idade = 19):
+    def __init__(self, *filhos, nome = None, idade = 19):
         self.idade = idade
         self.nome = nome
+        self.filhos =list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Rodrigues')
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Bernardo'
-    print(p.nome)
-    print(p.idade)
+    Bernardo = Pessoa(nome = 'Bernardo')
+    Antonio = Pessoa(Bernardo, nome = 'Antonio')
+    print(id(Bernardo))
+    print(Bernardo.cumprimentar())
+    print(Antonio.nome)
+    for filho in Antonio.filhos:
+        print(filho.nome)
+    Antonio.sobrenome = 'Rodrigues'
+    del Antonio.filhos
+    print(Antonio.__dict__)
+    print(Bernardo.__dict__)
