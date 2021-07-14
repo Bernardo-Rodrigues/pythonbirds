3e650420cda526ca490f52a272e4e5fda50fadf8
Bernardo-Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2021-07-14 10:14:26 -0300

    Adicionando sobrescrita de atributos e metodos através de herança

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -6,7 +6,7 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá, meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -16,9 +16,19 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_da_classe = super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
+
+
+class Mutante(Pessoa):
+    olhos = 3
+
 if __name__ == '__main__':
-    Bernardo = Pessoa(nome = 'Bernardo')
-    Antonio = Pessoa(Bernardo, nome = 'Antonio')
+    Bernardo = Mutante(nome = 'Bernardo')
+    Antonio = Homem(Bernardo, nome = 'Antonio')
     print(id(Bernardo))
     print(Bernardo.cumprimentar())
     print(Antonio.nome)
@@ -30,8 +40,13 @@ if __name__ == '__main__':
     del Antonio.olhos
     print(Antonio.__dict__)
     print(Bernardo.__dict__)
-    Pessoa.olhos = 3
     print(Antonio.olhos, Bernardo.olhos)
     print(id(Pessoa.olhos), id(Bernardo.olhos), id(Antonio.olhos))
     print(Pessoa.metodo_estatico(), Antonio.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_classe(), Antonio.nome_e_atributos_de_classe())
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa), isinstance(pessoa, Homem))
+    print(isinstance(Bernardo, Pessoa), isinstance(Bernardo, Homem))
+    print(Bernardo.olhos)
+    print(Antonio.cumprimentar())
+    print(Bernardo.cumprimentar())
